490298c6d04189669063006040ae91a409f89b87
Cristofer <cristofer_cd@outlook.es>
2019-08-18 12:08:25 +0200

    Set episode as seen when loaded

--- src/store/modules/videoPlayer.js
+++ src/store/modules/videoPlayer.js
@@ -14,6 +14,7 @@ const actions = {
     loadVideo: async ({ commit }, episode) => {
         omdb.loadVideo(episode.fileId).then(src => {
             episode.src = src
+            omdb.setSeen(episode.id, true)
             commit('setCurrentlyPlaying', episode)            
         })
     },
